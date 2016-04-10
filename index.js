8c15446771cee122e01548d6c10b03ef79fbe5ff
Lee Bousfield <ljbousfield@gmail.com>
2016-04-10 15:03:52 -0600

    Simplified error message

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ module.exports = function (mw, context, next) {
   if (context.wrappers) {
     context.wrappers.forEach(wrapper => mw = wrapper(mw));
   }
-  if (typeof mw !== 'function') throw new TypeError('Middleware must be a function!');
-  if (isGeneratorFunction(mw)) {
-    throw new TypeError('Middleware should not be a generator! ' +
+  if (typeof mw !== 'function' || isGeneratorFunction(mw)) {
+    throw new TypeError('Middleware must be a non-generator function! ' +
       'Are you sure you didn\'t forget a wrapper?');
   }
   return mw(context, next);
